fd8a9cc683ef0ac021821ee19fe9b84fb41a84c3
wangchunbin <474103319@qq.com>
2018-05-24 22:39:28 +0800

    更新版本号！

--- src/main/java/com/wcb/es/pojo/Employee.java
+++ src/main/java/com/wcb/es/pojo/Employee.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.util.Date;
 
 @Data
