1c816c7526efc3941a1eee4eee765f5c653ebf4f
wangchunbin <474103319@qq.com>
2018-05-26 14:56:56 +0800

    更新文件！

--- src/main/java/com/wcb/es/SpringbootdemoApplication.java
+++ src/main/java/com/wcb/es/SpringbootdemoApplication.java
@@ -3,6 +3,10 @@ package com.wcb.es;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Configuration注解的类上，来获取需要依赖注入的Bean；
+ * @ImportResource，来导入xml格式的配置文件；
+ */
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
--- src/main/java/com/wcb/es/controller/HtmlController.java
+++ src/main/java/com/wcb/es/controller/HtmlController.java
@@ -1,15 +1,20 @@
 package com.wcb.es.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HtmlController {
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     @RequestMapping("/goIndexHtml")
     public ModelAndView goIndexHtml() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.getModel().put("msg", "欢迎使用SpringBoot！");
+        modelAndView.getModel().put("msg", "欢迎访问SpringBoot:"+contextPath+"应用！");
         modelAndView.setViewName("index");
         return modelAndView;
     }
