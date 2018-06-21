a0eeb274582ea1721b2184ebc22ebbd96177f07c
wangchunbin <474103319@qq.com>
2018-06-21 20:05:07 +0800

    更新文件！

--- src/main/java/com/wcb/es/SpringbootdemoApplication.java
+++ src/main/java/com/wcb/es/SpringbootdemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Configuration注解的类上，来获取需要依赖注入的Bean；
- * @ImportResource，来导入xml格式的配置文件；
- * http://www.tianshouzhi.com/api/tutorials/springboot/82
+ * @ImportResource，来导入xml格式的配置文件； http://www.tianshouzhi.com/api/tutorials/springboot/82
  */
 @SpringBootApplication
 public class SpringbootdemoApplication {
